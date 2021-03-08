1d5bd9274f9664d74613523384d0bdef2fff1863
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-03-08 16:22:53 -0600

    fix: base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import vue from '@vitejs/plugin-vue';
  * @type {import('vite').UserConfig}
  */
 export default {
-    base: 'https://AndrewHeinke.github.io/30daysoftailwindcss/',
+    base: '',
     plugins: [vue()],
 };
