baa3a2e3a07fe7fc79f6ede18d8c8045a76c9f11
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-03-08 10:43:08 -0600

    feat: project set up and Day 1 challenge

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./src/**/*.html'],
+  },
+  darkMode: 'media',
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
