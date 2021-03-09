422bc59f650f070585bac387e8bef727cc4cfaf9
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-03-09 00:25:18 -0600

    fix: commented out image need to fix build

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
     <main>
         <div class="dark:text-white text-center">
-            <img class="w-48 mx-auto animate-pulse" src="img/claps.png" alt="clap it up" />
+            <!-- <img class="w-48 mx-auto animate-pulse" src="img/claps.png" alt="clap it up" /> -->
             <h1 class="text-6xl font-bold leading-tight mb-4">30 Days of Tailwind CSS</h1>
             <p class="text-xl">
                 A site to show all the challenge demos from the 30 days TailwindCSS challenge.
