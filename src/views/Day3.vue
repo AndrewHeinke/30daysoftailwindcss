f5fb383a8f9beb411e148a85383b56406d50bca0
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-03-09 16:03:44 -0600

    feat: meta img for social media posts

--- src/views/Day3.vue
+++ src/views/Day3.vue
@@ -16,7 +16,7 @@
                 </button>
                 <button
                     type="button"
-                    class="rounded-md border border-transparent px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mt-3 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm w-full sm:w-auto transition duration-500 ease-in-out inline-flex"
+                    class="rounded-md border border-transparent px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 mt-3 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm w-full sm:w-auto transition duration-500 ease-in-out inline-flex justify-center"
                 >
                     <svg
                         class="-ml-1 mr-2 h-5 w-5"
