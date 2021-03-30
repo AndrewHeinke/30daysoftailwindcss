736b575b7c1df5c4d90f96974510587c9b5aaeab
AndrewHeinke <andrew.j.heinke@gmail.com>
2021-03-30 10:19:09 -0500

    feat: day 8 challenge

--- src/views/Day8.vue
+++ src/views/Day8.vue
@@ -1,5 +1,68 @@
 <template>
     <main>
-        <div class="demo-container">content to come ...</div>
+        <div class="demo-container">
+            <div class="grid grid-cols-1 md:grid-cols-6 gap-4">
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    1
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    2
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    3
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    4
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    5
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    6
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    7
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    8
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    9
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    10
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    11
+                </div>
+                <div
+                    class="bg-blue-500 h-12 rounded-md flex items-center justify-center text-white text-2xl font-extrabold"
+                >
+                    12
+                </div>
+            </div>
+        </div>
     </main>
 </template>
